8bb36dd14b8d04f74de79d26cbbb6ac3c79090ea
Pranshu321 <pranshujain0111@gmail.com>
2022-03-25 19:45:14 +0530

    The react Todo List

---
+++ precache-manifest.38e595672a3085475e225fb20af5a18d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ae1d401a76ae9ac4711d6e2d21a1b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "f7f894d2379d1eae4dad",
+    "url": "/static/js/main.990fb050.chunk.js"
+  },
+  {
+    "revision": "164da21e09e43c8bb0c6",
+    "url": "/static/js/runtime-main.aa0ae0cd.js"
+  }
+]);
